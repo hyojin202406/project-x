20cbb0c576437b512d212e0fa1bee8f7e3f24c47
hj20220908 <hj20220908@gmail.com>
2024-06-22 23:02:50 +0900

    front-x commit

--- front-x/src/Components/HomeSection/ReplyModal.jsx
+++ front-x/src/Components/HomeSection/ReplyModal.jsx
@@ -1,14 +1,14 @@
-import { Button, Avatar } from '@mui/material';
-import { useFormik } from 'formik';
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Button, Avatar } from '@mui/material'
+import { useFormik } from 'formik'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box'
+import Modal from '@mui/material/Modal'
 
-import ImageIcon from '@mui/icons-material/Image';
-import FmdGoodIcon from '@mui/icons-material/FmdGood';
-import TagFacesIcon from '@mui/icons-material/TagFaces';
+import ImageIcon from '@mui/icons-material/Image'
+import FmdGoodIcon from '@mui/icons-material/FmdGood'
+import TagFacesIcon from '@mui/icons-material/TagFaces'
 
 const style = {
   position: 'absolute',
@@ -21,141 +21,150 @@ const style = {
   boxShadow: 24,
   p: 4,
   outline: 'none',
-  borderRadius: 4
-};
+  borderRadius: 4,
+}
 
-const ReplyModal = () => {
-  const [uploadingImage, setUploadingImage] = useState(false);
-  const [selectedImage, setSelectedImage] = useState(""); // 변경: 초기값 null로 설정
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+const ReplyModal = ({ handleClose, open }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  // const [open, setOpen] = React.useState(false)
+  // const handleOpen = () => setOpen(true)
+  // const handleClose = () => setOpen(false)
+  const [uploadingImage, setUploadingImage] = useState(false)
+  const [selectedImage, setSelectedImage] = useState('') // 변경: 초기값 null로 설정
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+  // const handleClick = (event) => {
+  //   setAnchorEl(event.currentTarget)
+  // }
 
   const navigate = useNavigate()
 
-  
   const handleSubmit = (values) => {
-    console.log("handle submit", values)
+    console.log('handle submit', values)
   }
 
   const formik = useFormik({
     initialValues: {
-      content: "",
-      image: "",
-      twitId: 4
+      content: '',
+      image: '',
+      twitId: 4,
     },
-    onSubmit: handleSubmit
+    onSubmit: handleSubmit,
   })
 
   const handleSelectImage = (event) => {
-    setUploadingImage(true);
-    const imgUrl = URL.createObjectURL(event.target.files[0]); // 파일 URL 생성
-    formik.setFieldValue("image", event.currentTarget.files[0]); // formik 상태 업데이트
-    setSelectedImage(imgUrl); // 선택된 이미지 업데이트
-    setUploadingImage(false);
-  };
+    setUploadingImage(true)
+    const imgUrl = URL.createObjectURL(event.target.files[0]) // 파일 URL 생성
+    formik.setFieldValue('image', event.currentTarget.files[0]) // formik 상태 업데이트
+    setSelectedImage(imgUrl) // 선택된 이미지 업데이트
+    setUploadingImage(false)
+  }
 
   return (
     <div>
-    {/* <Button onClick={handleOpen}>Open modal</Button> */}
-    <Modal
-      open={true}
-      onClose={handleClose}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
-      <Box sx={style}>
-      <div className='flex space-x-5'>
-        <Avatar
-          onClick={() => navigate(`/profile/${6}`)}
-          className='cursor-pointer'
-          alt="username"
-          src="/images/profile.png"
-        />
-        <div className='w-full'>
-          <div className='flex justify-between items-center'>
-
-            <div className='flex cursor-pointer items-center space-x-2'>
-              <span className='font-semibold'>Code With Hyojin</span>
-              <span className='text-gray-600'>@Hyojin . 2m</span>
-              <img className='ml-2 w-5 h-5' src="https://abs-0.twimg.com/emoji/v2/svg/1f31e.svg" alt="" />
-            </div>
-
-
-
-          </div>
-
-          <div className='mt-2'>
-            <div onClick={() => navigate(`/twit/${3}`)} className='cursor-pointer'>
-              <p className='mb-2 p-0'>hello !</p>
-            </div>
-
-          </div>
-        </div>
-
-        <section className='pb-10'>
-          <div className='flex space-x-5'>
+      {/* <Button onClick={handleOpen}>Open modal</Button> */}
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <div className="flex space-x-5">
             <Avatar
+              onClick={() => navigate(`/profile/${6}`)}
+              className="cursor-pointer"
               alt="username"
               src="/images/profile.png"
             />
-            <div className='w-full'>
-              <form onSubmit={formik.handleSubmit}>
-                <div>
-                  <input
-                    type="text"
-                    name='content'
-                    placeholder='What is happening?!'
-                    className='border-none outline-none text-xl bg-transparent'
-                    {...formik.getFieldProps("content")}
+            <div className="w-full">
+              <div className="flex justify-between items-center">
+                <div className="flex cursor-pointer items-center space-x-2">
+                  <span className="font-semibold">Code With Hyojin</span>
+                  <span className="text-gray-600">@Hyojin . 2m</span>
+                  <img
+                    className="ml-2 w-5 h-5"
+                    src="https://abs-0.twimg.com/emoji/v2/svg/1f31e.svg"
+                    alt=""
                   />
-                  {formik.errors.content && formik.touched.content && (
-                    <span className='text-red-500'>{formik.errors.content}</span>
-                  )}
                 </div>
-                <div>
-                  {selectedImage && (
-                    <img src={selectedImage} alt="Selected" style={{ maxWidth: '100%', maxHeight: '200px' }} />
-                  )}
+              </div>
+
+              <div className="mt-2">
+                <div
+                  onClick={() => navigate(`/twit/${3}`)}
+                  className="cursor-pointer"
+                >
+                  <p className="mb-2 p-0">hello !</p>
                 </div>
-                <div className='flex justify-between items-center mt-5'>
-                  <div className='flex space-x-5 items-center'>
-                    <label className='flex items-center space-x-2 rounded-md cursor-pointer'>
-                      <ImageIcon className='text-[#1d9bf0]' />
-                      <input
-                        type="file"
-                        name='imageFile'
-                        className='hidden'
-                        onChange={handleSelectImage}
-                        accept="image/*" // 이미지 파일만 선택 가능하도록 설정
-                      />
-                    </label>
-                    <FmdGoodIcon className='text-[#1d9bf0]' />
-                    <TagFacesIcon className='text-[#1d9bf0]' />
-                  </div>
-                  <div>
-                    <Button
-                      sx={{ width: "100%", borderRadius: "20px", paddingY: "8px", paddingX: "20px", bgcolor: "1e88e5" }}
-                      variant='contained'
-                      type='submit'
-                    >
-                      Tweet
-                    </Button>
-                  </div>
-                </div>
-              </form>
+              </div>
             </div>
           </div>
-        </section>
-      </div>
-      </Box>
-    </Modal>
-  </div>
+          <section className="py-10">
+            <div className="flex space-x-5">
+              <Avatar alt="username" src="/images/profile.png" />
+              <div className="w-full">
+                <form onSubmit={formik.handleSubmit}>
+                  <div>
+                    <input
+                      type="text"
+                      name="content"
+                      placeholder="What is happening?!"
+                      className="border-none outline-none text-xl bg-transparent"
+                      {...formik.getFieldProps('content')}
+                    />
+                    {formik.errors.content && formik.touched.content && (
+                      <span className="text-red-500">
+                        {formik.errors.content}
+                      </span>
+                    )}
+                  </div>
+                  <div>
+                    {selectedImage && (
+                      <img
+                        src={selectedImage}
+                        alt="Selected"
+                        style={{ maxWidth: '100%', maxHeight: '200px' }}
+                      />
+                    )}
+                  </div>
+                  <div className="flex justify-between items-center mt-5">
+                    <div className="flex space-x-5 items-center">
+                      <label className="flex items-center space-x-2 rounded-md cursor-pointer">
+                        <ImageIcon className="text-[#1d9bf0]" />
+                        <input
+                          type="file"
+                          name="imageFile"
+                          className="hidden"
+                          onChange={handleSelectImage}
+                          accept="image/*" // 이미지 파일만 선택 가능하도록 설정
+                        />
+                      </label>
+                      <FmdGoodIcon className="text-[#1d9bf0]" />
+                      <TagFacesIcon className="text-[#1d9bf0]" />
+                    </div>
+                    <div>
+                      <Button
+                        sx={{
+                          width: '100%',
+                          borderRadius: '20px',
+                          paddingY: '8px',
+                          paddingX: '20px',
+                          bgcolor: '1e88e5',
+                        }}
+                        variant="contained"
+                        type="submit"
+                      >
+                        Tweet
+                      </Button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </section>
+        </Box>
+      </Modal>
+    </div>
   )
 }
 
--- front-x/src/Components/HomeSection/TweetCard.jsx
+++ front-x/src/Components/HomeSection/TweetCard.jsx
@@ -1,73 +1,74 @@
-import { Button, Avatar, Menu, MenuItem  } from '@mui/material';
-import React from 'react'
-import RepeatIcon from '@mui/icons-material/Repeat';
-import { useNavigate } from 'react-router-dom';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import FileUploadIcon from '@mui/icons-material/FileUpload';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ReplyModal from './ReplyModal';
-
+import { Button, Avatar, Menu, MenuItem } from '@mui/material'
+import React, { useState } from 'react'
+import RepeatIcon from '@mui/icons-material/Repeat'
+import { useNavigate } from 'react-router-dom'
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import FileUploadIcon from '@mui/icons-material/FileUpload'
+import BarChartIcon from '@mui/icons-material/BarChart'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import ReplyModal from './ReplyModal'
 
 const TweetCard = () => {
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
 
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
+  const [openReplyModal, setOpenReplyModal] = useState(false)
+  const handleOpenReplyModal = () => setOpenReplyModal(true)
+  const handleCloseReplyModal = () => setOpenReplyModal(false)
+
   const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+    setAnchorEl(event.currentTarget)
+  }
   const handleClose = () => {
-    setAnchorEl(null);
-  };
-  
+    setAnchorEl(null)
+  }
+
   const navigate = useNavigate()
 
   const handleDeleteTweet = () => {
-    console.log("dlelete tweet")
-    handleClose();
+    console.log('dlelete tweet')
+    handleClose()
   }
 
   const handleEditTweet = () => {
-    console.log("edit tweet")
-    handleClose();
-  }
-  
-  const handleOpenReplyModel = () => {
-    console.log("open model")
+    console.log('edit tweet')
+    handleClose()
   }
 
   const handleCreateRetweet = () => {
-    console.log("handle create retweet ")
+    console.log('handle create retweet ')
   }
 
   const handleLiketweet = () => {
-    console.log("handle like tweet ")
+    console.log('handle like tweet ')
   }
-  
+
   return (
     <React.Fragment>
-
       {/* <div className='flex items-center font-semibold text-gray-700 py-2'>
         <RepeatIcon/>
         <p>You Retweet</p>
       </div> */}
 
-      <div className='flex space-x-5'>
+      <div className="flex space-x-5">
         <Avatar
           onClick={() => navigate(`/profile/${6}`)}
-          className='cursor-pointer'
+          className="cursor-pointer"
           alt="username"
           src="/images/profile.png"
         />
-        <div className='w-full'>
-          <div className='flex justify-between items-center'>
-
-            <div className='flex cursor-pointer items-center space-x-2'>
-              <span className='font-semibold'>Code With Hyojin</span>
-              <span className='text-gray-600'>@Hyojin . 2m</span>
-              <img className='ml-2 w-5 h-5' src="https://abs-0.twimg.com/emoji/v2/svg/1f31e.svg" alt="" />
+        <div className="w-full">
+          <div className="flex justify-between items-center">
+            <div className="flex cursor-pointer items-center space-x-2">
+              <span className="font-semibold">Code With Hyojin</span>
+              <span className="text-gray-600">@Hyojin . 2m</span>
+              <img
+                className="ml-2 w-5 h-5"
+                src="https://abs-0.twimg.com/emoji/v2/svg/1f31e.svg"
+                alt=""
+              />
             </div>
 
             <div>
@@ -78,7 +79,7 @@ const TweetCard = () => {
                 aria-expanded={open ? 'true' : undefined}
                 onClick={handleClick}
               >
-                <MoreHorizIcon/>
+                <MoreHorizIcon />
               </Button>
               <Menu
                 id="basic-menu"
@@ -93,54 +94,77 @@ const TweetCard = () => {
                 <MenuItem onClick={handleEditTweet}>Edit</MenuItem>
               </Menu>
             </div>
-
           </div>
 
-          <div className='mt-2'>
-            <div onClick={() => navigate(`/twit/${3}`)} className='cursor-pointer'>
-                <p className='mb-2 p-0'>hello !</p>
-                <img className='w-[28rem] border border-gray-400 p-5 rounded-md' src="https://plus.unsplash.com/premium_photo-1718747305176-8dab6d6abece?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <div className="mt-2">
+            <div
+              onClick={() => navigate(`/twit/${3}`)}
+              className="cursor-pointer"
+            >
+              <p className="mb-2 p-0">hello !</p>
+              <img
+                className="w-[28rem] border border-gray-400 p-5 rounded-md"
+                src="https://plus.unsplash.com/premium_photo-1718747305176-8dab6d6abece?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+              />
             </div>
-            <div className='py-5 flex flex-wrap justify-between items-center'>
-              <div className='space-x-3 flex items-center text-gray-600'>
-                <ChatBubbleOutlineIcon className='cursor-pointer' onClick={handleOpenReplyModel}/>
+            <div className="py-5 flex flex-wrap justify-between items-center">
+              <div className="space-x-3 flex items-center text-gray-600">
+                <ChatBubbleOutlineIcon
+                  className="cursor-pointer"
+                  onClick={handleOpenReplyModal}
+                />
                 <p>43</p>
               </div>
 
-              <div className={`${true ? "text-pink-600" : "text-gray-600"} space-x-3 flex items-center`}>
-                <RepeatIcon 
+              <div
+                className={`${true ? 'text-pink-600' : 'text-gray-600'} space-x-3 flex items-center`}
+              >
+                <RepeatIcon
                   onClick={handleLiketweet}
-                  className='cursor-pointer'/>
+                  className="cursor-pointer"
+                />
                 <p>54</p>
-
               </div>
 
-              <div className={`${true ? "text-pink-600" : "text-gray-600"} space-x-3 flex items-center`}>
-                {true? <FavoriteIcon 
-                  onClick={handleLiketweet}
-                  className='cursor-pointer'/> : <FavoriteBorderIcon 
-                  onClick={handleLiketweet}
-                  className='cursor-pointer'/>}
+              <div
+                className={`${true ? 'text-pink-600' : 'text-gray-600'} space-x-3 flex items-center`}
+              >
+                {true ? (
+                  <FavoriteIcon
+                    onClick={handleLiketweet}
+                    className="cursor-pointer"
+                  />
+                ) : (
+                  <FavoriteBorderIcon
+                    onClick={handleLiketweet}
+                    className="cursor-pointer"
+                  />
+                )}
                 <p>54</p>
-
               </div>
 
-              <div className='space-x-3 flex items-center text-gray-600'>
-                <BarChartIcon className='cursor-pointer' onClick={handleOpenReplyModel}/>
+              <div className="space-x-3 flex items-center text-gray-600">
+                <BarChartIcon
+                  className="cursor-pointer"
+                  onClick={handleOpenReplyModal}
+                />
                 <p>430</p>
               </div>
 
-              <div className='space-x-3 flex items-center text-gray-600'>
-                <FileUploadIcon className='cursor-pointer' onClick={handleOpenReplyModel}/>
+              <div className="space-x-3 flex items-center text-gray-600">
+                <FileUploadIcon
+                  className="cursor-pointer"
+                  onClick={handleOpenReplyModal}
+                />
               </div>
-
             </div>
           </div>
         </div>
       </div>
 
       <section>
-        <ReplyModal/>
+        <ReplyModal open={openReplyModal} handleClose={handleCloseReplyModal} />
       </section>
     </React.Fragment>
   )
